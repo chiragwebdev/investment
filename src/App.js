3754bd338a533e677696f081b2423c6a96e3cadd
chirag022dev <chiragkdesign@gmail.com>
2023-03-23 16:53:34 +0530

    a

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import {BrowserRouter } from 'react-router-dom'
 function App() {
   useEffect(() => {
     window.addEventListener("load", () => {
-      window.scrollTo(0, 0);
+      window.scrollTo(0, 1);
     });
   }, []);
 
